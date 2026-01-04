b491dcaa7db532681aaf5c3b78951be6fa6792fc
Chris Riebschlager <riebschlager@gmail.com>
2026-01-04 12:49:25 -0600

    stub out og meta tags

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const buildTime = new Date().toISOString();
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: "inject-build-time",
+      transformIndexHtml(html) {
+        return html.replace("%BUILD_TIME%", buildTime);
+      },
+    },
+  ],
   base: "/",
 });
